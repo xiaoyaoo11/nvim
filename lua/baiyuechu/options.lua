ed74b4712195aed0fa1f343fd18c74affac65903
xiaoyaoo11 <taitran411789@gmail.com>
2025-02-03 02:02:56 +0700

    update

--- lua/baiyuechu/options.lua
+++ lua/baiyuechu/options.lua
@@ -41,4 +41,7 @@ opt.splitbelow = true
 opt.iskeyword:append("-")
 
 -- add binaries installed by mason.nvim to path
+local is_windows = vim.fn.has("win32") ~= 0
+local sep = is_windows and "\\" or "/"
+local delim = is_windows and ";" or ":"
 vim.env.PATH = table.concat({ vim.fn.stdpath("data"), "mason", "bin" }, sep) .. delim .. vim.env.PATH
